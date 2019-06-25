64c16503160d42a8f58397300b9bcba23ac8cd89
tran ngoc tu <ngoctu17101991@gmail.com>
2019-06-25 15:24:12 +0700

    bo sung them may tinh nang cao

--- maytinh.js
+++ maytinh.js
@@ -3,17 +3,21 @@ let secondnumber = document.getElementById("secondnumber");
 let ketqua = document.getElementById("ketqua");
 
 function cong() {
+
     let tong = parseInt(firstnumber.value) + parseInt(secondnumber.value);
     ketqua.innerHTML = tong;
 }
+
 function tru() {
     let hieu = parseInt(firstnumber.value) - parseInt(secondnumber.value);
     ketqua.innerHTML = hieu;
 }
+
 function nhan() {
     let tich = parseInt(firstnumber.value) * parseInt(secondnumber.value);
     ketqua.innerHTML = tich;
 }
+
 function chia() {
     let thuong = parseInt(firstnumber.value) / parseInt(secondnumber.value);
     ketqua.innerHTML = thuong;
